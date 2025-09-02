78f153efe634b3ddab9a7c65e5bfa2aa39fa2c1d
yoyo <yoyo@yoyo.com>
2025-09-02 01:40:48 -0300

    Simplify App.tsx to fix build - revert to working funnel

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { DJElitePage } from './pages/DJElitePage';
-import { HomePage } from './pages/HomePage';
-import { CheckoutPage } from './pages/CheckoutPage';
-import { SuccessPage } from './pages/SuccessPage';
 import './index.css';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/funnel" element={<DJElitePage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/success" element={<SuccessPage />} />
-      </Routes>
-    </Router>
-  );
+  return <DJElitePage />;
 }
 
 export default App;
