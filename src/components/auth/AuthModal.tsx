1621956a68a4b130e89e3012a07d56b4c6aeec66
yoyo <yoyo@yoyo.com>
2025-10-07 03:26:19 -0300

    Fix AuthModal to use Clerk auth context

--- src/components/auth/AuthModal.tsx
+++ src/components/auth/AuthModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
+import { useAuth } from '../../contexts/ClerkAuthContext';
 import { XIcon } from '../../constants/platform';
 
 interface AuthModalProps {
